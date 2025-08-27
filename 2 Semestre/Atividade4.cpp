e6488f066537d1030b241e667611653955d22a6a
Guilherme motta <gui.motta6@gmail.com>
2025-08-26 22:49:49 -0300

    ref
    .

--- 2 Semestre/Atividade4.cpp
+++ 2 Semestre/Atividade4.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
-#include <windows.h>
-#include <string>
-#include <algorithm> 
-
 using namespace std;
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-
-    string frase;
-
+    char frase[100]; // Array para armazenar a frase
     cout << "Digite uma frase: ";
-    getline(cin, frase);
+    cin.getline(frase, 100); // Lê a frase inteira, incluindo espaços
 
     // a) Contar caracteres sem espaços
     int contagem = 0;
-    for (char c : frase) {
-        if (c != ' ') contagem++;
+    for (int i = 0; frase[i] != '\0'; i++) {
+        if (frase[i] != ' ') contagem++;
     }
     cout << "Quantidade de caracteres (sem espaços): " << contagem << endl;
 
-    // b) Frase em maiúsculas
-    string fraseMaiuscula = frase;
-    transform(fraseMaiuscula.begin(), fraseMaiuscula.end(), fraseMaiuscula.begin(), ::toupper);
-    cout << "Frase em maiúsculas: " << fraseMaiuscula << endl;
+    // b) Converter para maiúsculas
+    cout << "Frase em maiúsculas: ";
+    for (int i = 0; frase[i] != '\0'; i++) {
+        char c = frase[i];
+        if (c >= 'a' && c <= 'z') c -= 32; // Converte letra minúscula para maiúscula
+        cout << c;
+    }
+    cout << endl;
 
     // c) Frase ao contrário
-    string fraseInvertida = frase;
-    reverse(fraseInvertida.begin(), fraseInvertida.end());
-    cout << "Frase invertida: " << fraseInvertida << endl;
-
-    // d) Substituir 'a' por '@'
-    string fraseSubstituida = frase;
-    for (char &c : fraseSubstituida) {
-        if (c == 'a') c = '@';
-        if (c == 'A') c = '@'; 
+    cout << "Frase invertida: ";
+    int i = 0;
+    while (frase[i] != '\0') i++; 
+    for (int j = i - 1; j >= 0; j--) {
+        cout << frase[j];
     }
-    cout << "Frase com 'a' substituída por '@': " << fraseSubstituida << endl;
+    cout << endl;
+
+    // d) Substituir 'a' ou 'A' por '@'
+    cout << "Frase com 'a' substituída por '@': ";
+    for (int i = 0; frase[i] != '\0'; i++) {
+        if (frase[i] == 'a' || frase[i] == 'A') cout << "@";
+        else cout << frase[i];
+    }
+    cout << endl;
 
     return 0;
 }
